19a656c345d1bebac84177664a397d5ca1f9d686
Admin <Admin@DESKTOP-2HQUVUD>
2023-03-03 23:00:06 +0530

    By AKS

--- gitNNNN/src/com/cjc/rane.java
+++ gitNNNN/src/com/cjc/rane.java
@@ -1,24 +1,25 @@
 package com.cjc;
 
 public class rane {
+	
+	
+	
+	public void m1() {
+		
+		System.out.println("m1");
+		
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println("252525");
+		System.out.println("main");
 
-		System.out.println("111111111111");
-
-		System.out.println("22222222222");
+		
+		rane r =new rane ();
+		
+		r.m1();
 		
 		
-		public void m1(){
-		
-		
-		
-		}
-		
-		
-		
-
 	}
+	
 }
