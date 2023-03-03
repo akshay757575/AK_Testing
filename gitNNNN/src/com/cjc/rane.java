f9d6751e98abaa95b347692511de5965b609b382
Admin <Admin@DESKTOP-2HQUVUD>
2023-03-03 22:43:54 +0530

    by aaaaa

---
+++ gitNNNN/src/com/cjc/rane.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class rane {
+
+	public static void main(String[] args) {
+
+		System.out.println("252525");
+
+		System.out.println("111111111111");
+
+		System.out.println("22222222222");
+
+	}
+}
