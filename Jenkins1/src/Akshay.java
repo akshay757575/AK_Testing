d2ecf391d6fb226e98054424877d7bb51fecb054
Admin <Admin@DESKTOP-2HQUVUD>
2023-03-09 16:02:13 +0530

    By rane

---
+++ Jenkins1/src/Akshay.java
@@ -0,0 +1,11 @@
+
+public class Akshay {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("cjc karve nagar");
+		
+		
+	}
+}
---
+++ Jenkins1/src/cjc1.java
@@ -0,0 +1,7 @@
+
+public class cjc1 {
+
+	public static void main(String[] args) {
+		System.out.println("cjc pune");
+	}
+}
